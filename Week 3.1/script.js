54b94fb0409656180ff6647c1be6576a6b41b3c9
Ayazzsaifi <104493436+Ayazzsaifi@users.noreply.github.com>
2026-05-24 18:41:40 +0530

    week 3.1 DOM - todo list with numbering

--- Week 3.1/script.js
+++ Week 3.1/script.js
@@ -1,16 +1,20 @@
 const input=document.getElementById('input');
 const addBtn=document.getElementById('add-btn');
 const todoList= document.getElementById('todo-list');
+let count=0;
 
 addBtn.addEventListener("click" , function(){
+    let todoCount=todoList.children.length +1;
     let newTodo=document.createElement('div');
     let delBtn=document.createElement('button');
     delBtn.innerHTML="DELETE";
-    newTodo.innerHTML=input.value;
+    newTodo.innerHTML=todoCount+"." +" "+input.value;
     todoList.appendChild(newTodo);
     newTodo.appendChild(delBtn);
     delBtn.addEventListener("click", function(){
         newTodo.remove();
-    })
+    });
+    
     input.value="";
+
 });
