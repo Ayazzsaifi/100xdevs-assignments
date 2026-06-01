f571dcc5513d06b1c063d653b2f5b2da60d4830e
Ayazzsaifi <104493436+Ayazzsaifi@users.noreply.github.com>
2026-06-01 23:38:40 +0530

    Middleware ride age checker

--- week4.3/index.js
+++ week4.3/index.js
@@ -1,39 +1,64 @@
-const express=require('express')
+const express = require('express');
 const app = express();
-
-const users =[{
-    name:"harkirat",
-    firstkidneys:[{
-        healthy: false
+const users=[{
+    name:"kirat",
+    kidneys:[{
+        healthy:false
     }]
-}];
+}]
 
 app.use(express.json());
 
 app.get('/',function(req,res){
-    const harkiratkidneys=users[0].firstkidneys;
-    const numberrOfKidneys=harkiratkidneys.length;
-    const healthykidneys=harkiratkidneys.filter(function(kidney){
-        return kidney.healthy === true;
+    const kiratKidney=users[0].kidneys;
+    const numOfKidneys=kiratKidney.length;
+    const healthyKidneys=kiratKidney.filter(function(kidney){
+        return kidney.healthy==true;
     });
-    const numberOfhealtykidneys=healthykidneys.length;
-    const Unhealthykidneys=numberrOfKidneys-numberOfhealtykidneys;
+
+    const numofHealthykidneys=healthyKidneys.length;
+    const numOfUnhealthykidneys=numOfKidneys-numofHealthykidneys;
+
     res.json({
-        "total kidneys":numberrOfKidneys,
-        "Unhealthy Kidneys":numberOfhealtykidneys,
-        "healthy kidneys":healthykidneys
+        "total Nunber of kidneys":numOfKidneys,
+        "number of healthy Kidneys":numofHealthykidneys,
+        "number of unhealthy kidneys":numOfUnhealthykidneys
     });
+
+});
+
+app.post('/',function(req,res){
+    const ishealthy =req.body.ishealthy;
+    users[0].kidneys.push({
+        healthy:ishealthy
+    })
+
+    res.json({
+        msg:"done"
+    })
 });
 
 
-app.post('/addkidney',function(req,res){
-    const harkiratSecondKidneys=users[0].firstkidneys;
-    harkiratSecondKidneys.push({secondKidney:[{healthy:true}]});
-    res.json({message:"you kidney is transplanted ",
-        kidneys:users[0].firstkidneys
-    });
+app.put('/',function(req,res){
+    for(i=0;i<users[0].kidneys.length;i++){
+        users[0].kidneys[i].healthy=true;
+    }
+    res.json({})
+})
 
+app.delete('/',function(req,res){
+    const newkidney=[];
+    for(let i=0;i<users[0].kidneys.length;i++){
+        if(users[0].kidneys[i].healthy){
+            newkidney.push({
+                healthy:true
+            })
+        }
+    }
+    users[0].kidneys=newkidney;
+    res.json({
+        msg:"done"
+    })
 })
 
 app.listen(3000);
-console.log("running");
