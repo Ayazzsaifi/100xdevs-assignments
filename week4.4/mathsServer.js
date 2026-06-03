c5d6ac4dc785f6dfbeb8632d6abf8b8ec1ec8013
Ayazzsaifi <104493436+Ayazzsaifi@users.noreply.github.com>
2026-06-03 12:30:47 +0530

    Maths server with takes values from URL

---
+++ week4.4/mathsServer.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+app.get('/sum/:a/:b', function (req, res) {
+
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+    res.json({
+        ans: a + b
+    })
+})
+
+app.get('/subtract/:a/:b', function (req, res) {
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+    res.json({
+        ans: a - b
+    })
+})
+
+app.get('/divide/:a/:b', function (req, res) {
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+    if (b == 0) {
+        res.json({err: "b cnt be 0 "})
+    }
+    else {
+        res.json({
+            ans: a / b
+        })
+    }
+})
+
+
+app.get('/multiply/:a/:b', function (req, res) {
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+
+    res.json({
+        ans: a * b
+    })
+
+})
+
+
+app.listen(3000);
+console.log("working");
